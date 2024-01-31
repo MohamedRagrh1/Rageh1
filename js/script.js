ea2e81e8ab0bf9f2db20e2cf2156486c8d93750f
MohamedRagrh1 <157994071+MohamedRagrh1@users.noreply.github.com>
2024-02-01 00:31:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ===================== trping animation ================*/
+var typed = new Typed(".typing",{
+    strings:["","Frontend","Web Designer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
